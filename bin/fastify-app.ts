54736feb7e8fbe3f48803787e595d3c70b165393
Andre Macedo <dee.xd@hotmail.com>
2020-08-11 15:58:56 -0300

    Add fastify app stack

---
+++ bin/fastify-app.ts
@@ -0,0 +1,15 @@
+import * as dotenv from 'dotenv';
+import * as cdk from '@aws-cdk/core';
+
+import { FastifyAppStack } from '../lib/fastify-app-stack';
+
+dotenv.config();
+
+const app = new cdk.App();
+
+new FastifyAppStack(app, 'FastifyApp', {
+  env: {
+    account: process.env.AWS_ACCOUNT,
+    region: process.env.AWS_REGION,
+  },
+});
---
+++ lib/fastify-app-stack.ts
@@ -0,0 +1,27 @@
+import * as ec2 from '@aws-cdk/aws-ec2';
+import * as ecs from '@aws-cdk/aws-ecs';
+import * as ecs_patterns from '@aws-cdk/aws-ecs-patterns';
+import * as cdk from '@aws-cdk/core';
+
+class FastifyAppStack extends cdk.Stack {
+  constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    const vpc = new ec2.Vpc(this, 'FastifyAppVpc', { maxAzs: 1 });
+    const cluster = new ecs.Cluster(this, 'FastifyAppCluster', { vpc });
+
+    new ecs_patterns.ApplicationLoadBalancedFargateService(
+      this,
+      'FastifyAppFargateService',
+      {
+        cluster,
+        taskImageOptions: {
+          containerPort: 3000,
+          image: ecs.ContainerImage.fromRegistry('andreiox/fastify-test-drive'),
+        },
+      },
+    );
+  }
+}
+
+export { FastifyAppStack };
