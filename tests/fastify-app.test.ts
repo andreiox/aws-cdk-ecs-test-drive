d5097037178e2f7479ba58fd723a75b8c8dadc6c
Andre Macedo <dee.xd@hotmail.com>
2020-08-12 22:51:00 -0300

    Implement basic tests

--- tests/fastify-app.test.ts
+++ tests/fastify-app.test.ts
@@ -3,23 +3,26 @@ import * as cdk from '@aws-cdk/core';
 import { FastifyAppStack } from '../lib/fastify-app-stack';
 
 test('FastifyApp Stack', () => {
-  const app = new cdk.App();
-  const stack = new FastifyAppStack(app, 'FastifyAppStack');
+    const app = new cdk.App();
+    const stack = new FastifyAppStack(app, 'andreiox-fastify-test');
 
-  expectCDK(stack).to(haveResource('AWS::ElasticLoadBalancingV2::LoadBalancer'));
-  expectCDK(stack).to(
-    haveResource('AWS::ECS::TaskDefinition', (props: any) => {
-      const isImageCorrect =
-        props.ContainerDefinitions[0].Image === 'andreiox/fastify-test-drive';
-      const isPortCorrect =
-        props.ContainerDefinitions[0].PortMappings[0].ContainerPort === 3000;
+    expectCDK(stack).to(haveResource('AWS::ElasticLoadBalancingV2::LoadBalancer'));
+    expectCDK(stack).to(
+        haveResource('AWS::ECS::TaskDefinition', (props: any) => {
+            const isImageCorrect =
+                props.ContainerDefinitions[0].Image ===
+                'andreiox/fastify-test-drive';
+            const isPortCorrect =
+                props.ContainerDefinitions[0].PortMappings[0].ContainerPort === 3000;
 
-      return isImageCorrect && isPortCorrect;
-    }),
-  );
-  expectCDK(stack).to(
-    haveResource('AWS::ApplicationAutoScaling::ScalingPolicy', (props: any) => {
-      return props.TargetTrackingScalingPolicyConfiguration.TargetValue === 70;
-    }),
-  );
+            return isImageCorrect && isPortCorrect;
+        }),
+    );
+    expectCDK(stack).to(
+        haveResource('AWS::ApplicationAutoScaling::ScalingPolicy', (props: any) => {
+            return props.TargetTrackingScalingPolicyConfiguration.TargetValue === 70;
+        }),
+    );
+
+    expectCDK(stack).to(haveResource('AWS::CodePipeline::Pipeline'));
 });
