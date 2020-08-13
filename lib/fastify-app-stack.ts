c2b2eb00c82d557e926fa80c673c8d528c45b7cb
Andre Macedo <dee.xd@hotmail.com>
2020-08-12 22:29:11 -0300

    Refactor constructor

--- lib/fastify-app-stack.ts
+++ lib/fastify-app-stack.ts
@@ -9,98 +9,137 @@ import * as codebuild from '@aws-cdk/aws-codebuild';
 
 dotenv.config();
 
+interface StageInterface {
+    artifact: codepipeline.Artifact;
+    stage: codepipeline.IStage;
+}
+
 class FastifyAppStack extends cdk.Stack {
-  constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+    constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
+        super(scope, id, props);
 
-    const vpc = new ec2.Vpc(this, 'FastifyAppVpc', { maxAzs: 2 });
-    const cluster = new ecs.Cluster(this, 'FastifyAppCluster', { vpc });
+        const vpc = new ec2.Vpc(this, 'FastifyAppVpc', { maxAzs: 2 });
+        const cluster = new ecs.Cluster(this, 'FastifyAppCluster', { vpc });
 
-    const fargateService = new ecs_patterns.ApplicationLoadBalancedFargateService(
-      this,
-      'FastifyAppFargateService',
-      {
-        cluster,
-        taskImageOptions: {
-          containerName: 'fastify-app',
-          containerPort: 3000,
-          image: ecs.ContainerImage.fromRegistry('andreiox/fastify-test-drive'),
-        },
-      },
-    );
+        const fargateService = this.createFargateService(cluster);
 
-    const scalling = fargateService.service.autoScaleTaskCount({ maxCapacity: 2 });
-    scalling.scaleOnCpuUtilization('CpuScaling', {
-      targetUtilizationPercent: 70,
-      scaleInCooldown: cdk.Duration.seconds(60),
-      scaleOutCooldown: cdk.Duration.seconds(60),
-    });
+        const pipeline = new codepipeline.Pipeline(this, 'FastifyAppPipeline', {
+            pipelineName: 'fastify-app-pipeline',
+        });
 
-    const pipeline = new codepipeline.Pipeline(this, 'FastifyAppPipeline', {
-      pipelineName: 'fastify-app-pipeline',
-    });
+        const source = this.createSourceStage(pipeline);
+        const build = this.createBuildStage(pipeline, source);
+        this.createDeployStage(pipeline, build, fargateService.service);
+    }
 
-    const sourceArtifact = new codepipeline.Artifact();
-    const sourceAction = new codepipeline_actions.GitHubSourceAction({
-      actionName: 'GitHub',
-      branch: 'aws-pipeline-support',
-      output: sourceArtifact,
-      oauthToken: cdk.SecretValue.plainText(process.env.GITHUB_TOKEN!),
-      trigger: codepipeline_actions.GitHubTrigger.POLL,
-      owner: process.env.GITHUB_OWNER!,
-      repo: process.env.GITHUB_REPO!,
-    });
+    createFargateService(
+        cluster: ecs.ICluster,
+    ): ecs_patterns.ApplicationLoadBalancedFargateService {
+        const fargateService = new ecs_patterns.ApplicationLoadBalancedFargateService(
+            this,
+            'FastifyAppFargateService',
+            {
+                cluster,
+                taskImageOptions: {
+                    containerName: 'fastify-app',
+                    containerPort: 3000,
+                    image: ecs.ContainerImage.fromRegistry(
+                        'andreiox/fastify-test-drive',
+                    ),
+                },
+            },
+        );
 
-    const sourceStage = pipeline.addStage({
-      stageName: 'Source',
-      actions: [sourceAction],
-    });
+        this.createAutoScaleForService(fargateService.service);
 
-    const buildArtifact = new codepipeline.Artifact();
-    const buildProject = new codebuild.PipelineProject(this, 'FastifyAppBuild', {
-      projectName: 'fastify-app-build',
-      environment: {
-        computeType: codebuild.ComputeType.SMALL,
-        buildImage: codebuild.LinuxBuildImage.STANDARD_4_0,
-        privileged: true,
-        environmentVariables: {
-          DOCKER_HUB_USER: { value: process.env.DOCKER_HUB_USER! },
-          DOCKER_HUB_PASSWORD: { value: process.env.DOCKER_HUB_PASSWORD! },
-          IMAGE_REPO_NAME: { value: process.env.IMAGE_REPO_NAME! },
-          IMAGE_TAG: { value: process.env.IMAGE_TAG! },
-        },
-      },
-    });
+        return fargateService;
+    }
 
-    const buildStage = pipeline.addStage({
-      stageName: 'Build',
-      actions: [
-        new codepipeline_actions.CodeBuildAction({
-          actionName: 'Build',
-          project: buildProject,
-          input: sourceArtifact,
-          outputs: [buildArtifact],
-        }),
-      ],
-      placement: {
-        justAfter: sourceStage,
-      },
-    });
+    createAutoScaleForService(service: ecs.FargateService): void {
+        const scalling = service.autoScaleTaskCount({ maxCapacity: 2 });
+        scalling.scaleOnCpuUtilization('CpuScaling', {
+            targetUtilizationPercent: 70,
+            scaleInCooldown: cdk.Duration.seconds(60),
+            scaleOutCooldown: cdk.Duration.seconds(60),
+        });
+    }
 
-    pipeline.addStage({
-      stageName: 'Deploy',
-      actions: [
-        new codepipeline_actions.EcsDeployAction({
-          actionName: 'Deploy',
-          service: fargateService.service,
-          input: buildArtifact,
-        }),
-      ],
-      placement: {
-        justAfter: buildStage,
-      },
-    });
-  }
+    createSourceStage(pipeline: codepipeline.Pipeline): StageInterface {
+        const artifact = new codepipeline.Artifact();
+
+        const stage = pipeline.addStage({
+            stageName: 'Source',
+            actions: [
+                new codepipeline_actions.GitHubSourceAction({
+                    actionName: 'GitHub',
+                    branch: 'aws-pipeline-support',
+                    output: artifact,
+                    oauthToken: cdk.SecretValue.plainText(process.env.GITHUB_TOKEN!),
+                    trigger: codepipeline_actions.GitHubTrigger.POLL,
+                    owner: process.env.GITHUB_OWNER!,
+                    repo: process.env.GITHUB_REPO!,
+                }),
+            ],
+        });
+
+        return { artifact, stage };
+    }
+
+    createBuildStage(pipeline: codepipeline.Pipeline, source: StageInterface) {
+        const artifact = new codepipeline.Artifact();
+
+        const buildProject = new codebuild.PipelineProject(this, 'FastifyAppBuild', {
+            projectName: 'fastify-app-build',
+            environment: {
+                computeType: codebuild.ComputeType.SMALL,
+                buildImage: codebuild.LinuxBuildImage.STANDARD_4_0,
+                privileged: true,
+                environmentVariables: {
+                    DOCKER_HUB_USER: { value: process.env.DOCKER_HUB_USER! },
+                    DOCKER_HUB_PASSWORD: { value: process.env.DOCKER_HUB_PASSWORD! },
+                    IMAGE_REPO_NAME: { value: process.env.IMAGE_REPO_NAME! },
+                    IMAGE_TAG: { value: process.env.IMAGE_TAG! },
+                },
+            },
+        });
+
+        const stage = pipeline.addStage({
+            stageName: 'Build',
+            actions: [
+                new codepipeline_actions.CodeBuildAction({
+                    actionName: 'Build',
+                    project: buildProject,
+                    input: source.artifact,
+                    outputs: [artifact],
+                }),
+            ],
+            placement: {
+                justAfter: source.stage,
+            },
+        });
+
+        return { artifact, stage };
+    }
+
+    createDeployStage(
+        pipeline: codepipeline.Pipeline,
+        build: StageInterface,
+        service: ecs.FargateService,
+    ) {
+        pipeline.addStage({
+            stageName: 'Deploy',
+            actions: [
+                new codepipeline_actions.EcsDeployAction({
+                    actionName: 'Deploy',
+                    service: service,
+                    input: build.artifact,
+                }),
+            ],
+            placement: {
+                justAfter: build.stage,
+            },
+        });
+    }
 }
 
 export { FastifyAppStack };
